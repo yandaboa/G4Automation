fdb55678a2885da53b01c0a413955b35f37211b3
Yanda Bao <yandabaodav@gmail.com>
2023-03-09 13:03:05 -0800

    student form is finished

--- src/views/StudentForm.vue
+++ src/views/StudentForm.vue
@@ -1,7 +1,7 @@
 <script>
 import { auth } from '../main';
 import { ref } from 'vue';
-import MultiSelect from 'vue-multiselect';
+import MultiSelect from '@vueform/multiselect';
 
 export default {
     components: {MultiSelect},
@@ -11,12 +11,13 @@ export default {
             selectedGrade: "",
             hasDoneG4: "",
             scienceClasses: null,
+            studentRequest: "",
             options: ["IB Bio", "IB Physics", "IB Comp Sci", "IB Chemistry", "IB Sports Sci"]
         }
     },
     methods: {
         formFilledOut(){
-            if(this.scienceClasses.length > 0 && name.value != "" && grade.value != "" && studentRequest.value != "" && hasDoneG4 != ""){
+            if(this.scienceClasses != null && this.name != "" && this.selectedGrade != "" &&  this.studentRequest != "" && this.hasDoneG4 != ""){
                 return false;
             }
             return true;
@@ -25,6 +26,10 @@ export default {
             console.log(this.name);
             console.log(this.selectedGrade);
             console.log(this.hasDoneG4);
+            this.scienceClasses.forEach(element => {
+                console.log(element);
+            });
+            console.log(this.studentRequest);
         }
     }
 }
@@ -35,32 +40,45 @@ export default {
         <div id = "form">
             <div class="studentForm">
                 <form @submit.prevent="submitForm">
+                    <h1 id="title">Student Form</h1>
                     <input class="inputFields"
                     type="text" 
-                    placeholder="Full name..."
+                    placeholder="Full name"
                     v-model="name"
                     />
-                    <label for="grade" class="gradeHeader">Choose your grade level:</label>
-                    <select v-model="selectedGrade" id="cars">
-                        <option value=""></option>
-                        <option value="11">11th</option>
-                        <option value="12">12th</option>
-                    </select>
-                    <label for="pastExperience" class="gradeHeader">Have you completed a G4 project in the past?</label>
-                    <select v-model="hasDoneG4" id="pastExp">
-                        <option value=""></option>
-                        <option value="yes">yes</option>
-                        <option value="no">no</option>
-                    </select>
-                    <div>
-                        <label class="typo__label">Simple select / dropdown</label>
-                        <multiselect v-model="scienceClasses"  :options="options">
-                        </multiselect>
+                    <br/>
+                    <div id="selector">
+                        <label for="grade" class="gradeHeader regular-font">Choose your grade level:</label>
+                        <select v-model="selectedGrade">
+                            <option value=""></option>
+                            <option value="11">11th</option>
+                            <option value="12">12th</option>
+                        </select>
+                    </div>
+                    <div id="selector">
+                        <label for="pastExperience" class="gradeHeader regular-font">Have you completed a G4 project in the past?</label>
+                        <select v-model="hasDoneG4" id="pastExp">
+                            <option value=""></option>
+                            <option value="yes">yes</option>
+                            <option value="no">no</option>
+                        </select>
+                    </div>
+                    <div id="selector">
+                        <label class="typo__label regular-font">Select all current IB science classes you are taking</label>
+                        <MultiSelect v-model="scienceClasses" mode="multiple" :close-on-select="false" :options="options" class="regular-font">
+                        </MultiSelect>
                         <!-- <pre class="language-json"><code>{{ scienceClasses  }}</code></pre> -->
                     </div>
-                    <button type="submit" :disabled="formFilledOut">
+                    <input class="inputFields" id="request"
+                    type="text" 
+                    placeholder="Student name"
+                    v-model="studentRequest"
+                    />
+                    <br /> <br /> <br />
+                    <button type="submit" :disabled="false">
                         Submit
                     </button>
+                    <p id="formInstructions" class="regular-font">{{!formFilledOut() ? "" : "Please fill out form completely"}}</p>
                 </form>
             </div>
         </div>
@@ -72,17 +90,36 @@ export default {
 <style scoped>
 .inputFields {
     font-family: Inter;
-    font-size: 1vw;
-    margin-left: 28vw;
-    margin-bottom: 3vh;
+    font-size: 1.25vw;
+    margin-left: 5vw;
+    margin-top: 3vh;
     border-radius: 0.3vw;
+}
+#request {
+    margin-top: 10vw;
+}
+#title {
+    margin-left: 5vw;
+}
+#selector {
+    margin-left: 5vw;
+    margin-top: 2vh;
+    padding-bottom: 0vh;
+    height: 25px;
+}
+form {
+    margin-top: 20vh;
 }
 select {
     font-family: Inter;
-    font-size: 1vw;
-    margin-left: 28vw;
+    font-size: 1.25vw;
+    margin-left: 3vw;
     margin-bottom: 3vh;
     border-radius: 0.3vw;
+}
+#formInstructions{
+    width: fit-content;
+    margin-left: 5vw;
 }
 #container {
     width: 100vw;
@@ -93,6 +130,9 @@ select {
 }
 .gradeHeader {
 }
+#pastExp {
+
+}
 #form {
     margin-left: 20vw;
     width: 60vw;
@@ -101,17 +141,23 @@ select {
     overflow: hidden;
 }
 .studentForm {
-    border: 5px solid black;
+    /* border: 5px solid black; */
     margin-top: 5vh;
     margin-left: 2.5vw;
     height: 90vh;
     width: 55vw;
 }
+.regular-font{
+    font-family: Inter;
+    font-size: 1.25vw;
+}
+
 button {
-    margin-left: 33vw;
-    font-size: 1vw;
+    margin-left: 5vw;
+    font-family: Inter;
+    font-size: 1.25vw;
     border-radius: 0.3vw;
-    margin-bottom: 2vh;
+    margin-bottom: 1vh;
     background-color: green;
 }
 </style>
